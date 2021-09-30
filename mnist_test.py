9815f4f9be8fd639be548010f2bbc65ac09a0cb1
Mo Tiwari <motiwari@stanford.edu>
2021-09-30 16:43:47 -0700

    KFAC working in 33 hyperepochs

--- mnist_test.py
+++ mnist_test.py
@@ -278,40 +278,41 @@ def KFAC_optimize(args, model, train_loader, val_loader, hyper_optimizer, kfac_o
     dLv_dw /= (batch_idx + 1) # TODO (@Mo): This is very bad, because it does not account for a potentially uneven batch at the end
 
     # Calculate preconditioner  v1*(inverse Hessian approximation) [orange term in Figure 2]
-    assert args.hessian == 'KFAC', f"Passed {args.hessian}, not a valid choice. Need to choose KFAC"
-    # model.zero_grad()
-    flat_pre_conditioner = torch.zeros(num_weights).cuda()
-    for batch_idx, (x, y) in enumerate(train_loader):
-        model.train()
-        model.zero_grad()
-        hyper_optimizer.zero_grad()
-        x, y = prepare_data(args, x, y)
-        train_loss, _ = batch_loss(args, model, x, y, model, train_loss_func)
-        # TODO (JON): Probably don't recompute - use create_graph and retain_graph?
-        dLt_dw = grad(train_loss, model.parameters(), create_graph=True)
-        flat_dLt_dw = gather_flat_grad(dLt_dw)
+    if args.hessian == 'KFAC':
+        print(f"Passed {args.hessian}, not a valid choice. Need to choose KFAC")
+        # model.zero_grad()
+        flat_pre_conditioner = torch.zeros(num_weights).cuda()
+        for batch_idx, (x, y) in enumerate(train_loader):
+            model.train()
+            model.zero_grad()
+            hyper_optimizer.zero_grad()
+            x, y = prepare_data(args, x, y)
+            train_loss, _ = batch_loss(args, model, x, y, model, train_loss_func)
+            # TODO (JON): Probably don't recompute - use create_graph and retain_graph?
+            dLt_dw = grad(train_loss, model.parameters(), create_graph=True)
+            flat_dLt_dw = gather_flat_grad(dLt_dw)
 
-        current = 0
-        for m in model.modules():
-            if m.__class__.__name__ in ['Linear', 'Conv2d']:
-                # kfac_opt.zero_grad()
-                if m.__class__.__name__ == 'Conv2d':
-                    size0, size1 = m.weight.size(0), m.weight.view(m.weight.size(0), -1).size(1)
-                else:
-                    size0, size1 = m.weight.size(0), m.weight.size(1)
-                mod_size1 = size1 + 1 if m.bias is not None else size1 # TODO (@Mo): Lol
-                shape = (size0, (mod_size1))
-                size = size0 * mod_size1
-                pre_conditioner = kfac_opt._get_natural_grad(m, dLv_dw[current:current + size].view(shape), KFAC_damping) # TODO (Mo): What is this?
-                flat_pre_conditioner[current: current + size] = gather_flat_grad(pre_conditioner)
-                current += size
-        model.zero_grad()
-        hyper_optimizer.zero_grad()
-        flat_dLt_dw.backward(flat_pre_conditioner)
-        total_dLv_dlambda -= get_hyper_train(args, model).grad
-        if batch_idx >= args.train_batch_num:
-            break
-    total_dLv_dlambda /= (batch_idx + 1) # TODO (@Mo): This is very bad, because it does not account for a potentially uneven batch at the end
+            current = 0
+            for m in model.modules():
+                if m.__class__.__name__ in ['Linear', 'Conv2d']:
+                    # kfac_opt.zero_grad()
+                    if m.__class__.__name__ == 'Conv2d':
+                        size0, size1 = m.weight.size(0), m.weight.view(m.weight.size(0), -1).size(1)
+                    else:
+                        size0, size1 = m.weight.size(0), m.weight.size(1)
+                    mod_size1 = size1 + 1 if m.bias is not None else size1 # TODO (@Mo): Lol
+                    shape = (size0, (mod_size1))
+                    size = size0 * mod_size1
+                    pre_conditioner = kfac_opt._get_natural_grad(m, dLv_dw[current:current + size].view(shape), KFAC_damping) # TODO (Mo): What is this?
+                    flat_pre_conditioner[current: current + size] = gather_flat_grad(pre_conditioner)
+                    current += size
+            model.zero_grad()
+            hyper_optimizer.zero_grad()
+            flat_dLt_dw.backward(flat_pre_conditioner)
+            total_dLv_dlambda -= get_hyper_train(args, model).grad
+            if batch_idx >= args.train_batch_num:
+                break
+        total_dLv_dlambda /= (batch_idx + 1) # TODO (@Mo): This is very bad, because it does not account for a potentially uneven batch at the end
 
 
     # Compute direct gradient of dLv_dlambda. This is usually 0.
@@ -319,6 +320,7 @@ def KFAC_optimize(args, model, train_loader, val_loader, hyper_optimizer, kfac_o
     direct_dLv_dlambda = torch.zeros(get_hyper_train(args, model).size(0))
     if args.cuda:
         direct_dLv_dlambda = direct_dLv_dlambda.cuda()
+
     model.train()
     for batch_idx, (x_val, y_val) in enumerate(val_loader):
         model.zero_grad()
